791fc65a03cbde3e2dd92bc03bc2c2a24514999b
Сырбек Олжас Бекенулы <olzhas.syrbek@ffin.kz>
2022-08-10 17:38:58 +0600

    commit

--- src/main/java/com/ntsdesign/testtask/controller/CommandController.java
+++ src/main/java/com/ntsdesign/testtask/controller/CommandController.java
@@ -1,11 +1,27 @@
 package com.ntsdesign.testtask.controller;
 
+import com.ntsdesign.testtask.model.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class CommandController {
 
+    private SimpMessagingTemplate messagingTemplate;
 
+    @Autowired
+    public CommandController(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
 
+    @MessageMapping("/message")
+    public void processMessage() {
+        System.out.println("dasdasdasd");
+       // messagingTemplate.convertAndSendToUser();
+
+    }
 
 }
